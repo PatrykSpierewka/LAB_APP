2e71d0b11fa79f93fb899cf08d5cf9700bbe27f3
Aleksandra Borowska <olkaborowska333@wp.pl>
2024-06-09 21:44:53 +0200

    patient_home

--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('doctor/', views.base, name='doctor'),
-    path('labmember/', views.base, name='labmember'),
+    path('doctor/', views.doctor, name='doctor'),
+    path('labmember/', views.labmember, name='labmember'),
     path('addresult/', views.addresult, name='addresult'),
     path('patient/', views.base, name='patient'),
     path('patient_home/', views.patient_home, name='patient_home'),
     path('base/', views.base, name='base'),
     path('login/', views.login_view, name='login_view'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import SignUpForm, LoginForm, TestPackageForm,User # AddResult
-from django.contrib.auth import authenticate, login
-from .models import Patients
+from django.contrib.auth import authenticate, login, logout
+from .models import Patients,Tests,Results
 
 def index(request):
     return render(request, 'index.html')
@@ -30,13 +30,13 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None and user.is_doctor:
                 login(request, user)
-                return redirect('doctor')
+                return redirect('base')
             elif user is not None and user.is_labmember:
                 login(request, user)
-                return redirect('labmember')
+                return redirect('base')
             elif user is not None and user.is_patient:
                 login(request, user)
-                return redirect('patient')
+                return redirect('base')
             else:
                 msg = 'invalid credentials'
         else:
@@ -50,20 +50,43 @@ def doctor(request):
     return render(request, 'doctor.html')
 
 def labmember(request):
-    return render(request, 'labmember.html')
+
+    print(f'Labmember view {request.method}')
+    if request.method == "GET":
+        tests=list(Tests.objects.values())
+        context={}
+        for idx,test in enumerate(tests):
+            result_id=test['result_id_id']
+            results=list(Results.objects.filter(result_id=result_id).values())
+            print(f'Result {results}')
+            for result in results:
+                for key in result.keys():
+                    test.update({key:result[key]})
+                context[f'test{idx}']=test
+              
+        print(f'Context {context}')
+        #pacjenci = Patients.objects.filter(name="Marek").values_list("health_id", flat=True).first()
+    return render(request, 'labmember.html',{'tests':context})
 
 def base(request):
-    is_doctor,is_labmember,is_patient = list(User.objects.filter(is_active=1).values_list('is_doctor','is_labmember','is_patient'))[0]
-    print(f'Actve user {is_doctor},{is_labmember},{is_patient}')
-    active_user=''
-    if is_doctor==True:
-        active_user='doctor'
-    elif is_labmember==True:
-        active_user='labmember'
-    else:
-        active_user='patient'
-    context={'active_user':active_user}
-    return render(request, 'base.html',context)
+    if request.user.is_authenticated:
+        is_doctor = request.user.is_doctor
+        is_labmember = request.user.is_labmember
+        is_patient = request.user.is_patient
+        print(f'Active user {is_doctor},{is_labmember},{is_patient}')
+        active_user = ''
+        if is_doctor==True:
+            active_user='doctor'
+        elif is_labmember==True:
+            active_user='labmember'
+        else:
+            active_user='patient'
+
+        context = {'active_user': active_user}
+        return render(request, 'base.html', context)
+    return render(request, 'base.html')
+
+    
 
 def patient_home(request):
     return render(request, 'patient_home.html')
@@ -80,6 +103,9 @@ def patient(request):
         form = TestPackageForm()
     return render(request, 'patient.html', {'form': form})
 
+def user_logout(request):
+    logout(request)
+    return redirect('/')
 
 def addresult(request):
     print("Widok wywołany")  # Debug: Sprawdź, czy widok jest wywoływany
